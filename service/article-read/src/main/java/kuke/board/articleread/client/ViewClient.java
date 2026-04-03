02ebcc0dd6c35b0299d467b82903eb1e0577c898
jihyungkim0011 <jihyungkim@MacBook-Air.local>
2026-04-03 12:26:23 +0900

    add: ArticleRead Service - OptimizedCache

--- service/article-read/src/main/java/kuke/board/articleread/client/ViewClient.java
+++ service/article-read/src/main/java/kuke/board/articleread/client/ViewClient.java
@@ -1,6 +1,7 @@
 package kuke.board.articleread.client;
 
 import jakarta.annotation.PostConstruct;
+import kuke.board.articleread.cache.OptimizedCacheable;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
@@ -21,7 +22,9 @@ public class ViewClient {
         restClient = RestClient.create(viewServiceUrl);
     }
 
-    @Cacheable(key = "#articleId", value = "articleViewCount") // **
+
+//    @Cacheable(key = "#articleId", value = "articleViewCount") // **
+    @OptimizedCacheable(type = "articleViewCount", ttlSeconds = 1)
     public long count(Long articleId) {
         log.info("[ViewClient.count] articleId={}", articleId);
         try {
